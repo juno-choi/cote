d75381039b0f60b2a6a3e0235a1f695918d687a8
juno-choi <ililil9482@naver.com>
2025-03-21 00:00:46 +0900

    같은 숫자는 싫어
    https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=java

---
+++ src/main/java/kr/co/simol/structure/m12906/Solution.java
@@ -0,0 +1,52 @@
+package kr.co.simol.structure.m12906;
+
+import java.util.*;
+
+public class Solution {
+    /**
+     * stack을 활용하여 중복수를 제거하는 방식 사용
+     */
+    public int[] solution(int []arr) {
+
+        Checker checker = Checker.create();
+        checker.addAll(arr);
+
+        return checker.getAnswer();
+    }
+}
+
+class Checker {
+    Stack<Integer> stack;
+
+    protected Checker() {
+        this.stack = new Stack();
+    }
+
+    public static Checker create() {
+        return new Checker();
+    }
+
+    public void addAll(int[] arr) {
+        for (int i : arr) {
+            if (this.stack.size() == 0) {
+                this.stack.push(i);
+                continue;
+            }
+
+            int peek = this.stack.peek();
+            if (peek != i) {
+                this.stack.push(i);
+            }
+        }
+    }
+
+    public int[] getAnswer() {
+        int stackSize = this.stack.size();
+        int[] answer = new int[stackSize];
+        for (int i=stackSize-1; i>=0; i--) {
+            answer[i] = stack.pop();
+        }
+
+        return answer;
+    }
+}
