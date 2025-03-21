9bf69c72d4a54e041d1a628bfcb8e2ee524c577b
juno-choi <ililil9482@naver.com>
2025-03-22 08:41:50 +0900

    타겟 넘버 (다시 풀기2)
    https://school.programmers.co.kr/learn/courses/30/lessons/43165

--- src/main/java/kr/co/simol/dfs/m43165/Solution.java
+++ src/main/java/kr/co/simol/dfs/m43165/Solution.java
@@ -5,40 +5,35 @@ package kr.co.simol.dfs.m43165;
  * 예전에는 다 이해했는데 이젠 이해가 안된다... 다른 문제를 풀면서 더 이해해보자.
  */
 public class Solution {
-    static int answer;
     public int solution(int[] numbers, int target) {
-        Calculator calculator = new Calculator();
-        calculator.updateData(numbers, target);
-        calculator.dfs(0, 0);
-        return calculator.getAnswer();
-    }
 
+        Checker checker = new Checker(numbers);
+        checker.dfs(0, 0, target);
+
+        return checker.getAnswer();
+    }
 }
 
-class Calculator{
-    private int answer;
+class Checker {
     private int[] numbers;
-    private int target;
+    private int answer;
 
-    public Calculator() {
-        this.answer = 0;
-    }
-
-    public void updateData(int[] numbers, int target) {
+    public Checker (int[] numbers){
         this.numbers = numbers;
-        this.target = target;
     }
 
-    public void dfs(int level, int sum) {
+    public void dfs(int level, int sum, int target) {
+        int numbersLength = this.numbers.length;
 
-        if (level == this.numbers.length) {
+        if (level == numbersLength) {
             if (sum == target) {
-                this.answer++;
+                answer++;
             }
             return ;
         }
-        dfs(level+1, sum + this.numbers[level]);
-        dfs(level+1, sum - this.numbers[level]);
+
+        dfs(level+1, sum+this.numbers[level], target);
+        dfs(level+1, sum-this.numbers[level], target);
     }
 
     public int getAnswer() {
