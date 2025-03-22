dc354f135c1a7161639b35995a6872bb6e62d83e
juno-choi <ililil9482@naver.com>
2025-03-23 08:58:48 +0900

    주식 가격
    https://school.programmers.co.kr/learn/courses/30/lessons/42584

---
+++ src/main/java/kr/co/simol/practice/test1/m42584/Solution.java
@@ -0,0 +1,58 @@
+package kr.co.simol.practice.test1.m42584;
+
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] prices = {1, 2, 3, 2, 3};
+        solution.solution(prices);
+    }
+    public int[] solution(int[] prices) {
+        Checker checker = new Checker();
+        checker.addAll(prices);
+        checker.check(prices);
+        return checker.getAnswer();
+    }
+}
+
+
+
+class Checker {
+    private Queue<Integer> queue;
+    private int[] answer;
+
+    public Checker() {
+        this.queue = new LinkedList<>();
+    }
+
+    public void addAll(int[] prices) {
+        for (int i=0; i<prices.length; i++) {
+            int price = prices[i];
+            this.queue.offer(price);
+        }
+
+        this.answer = new int[prices.length];
+    }
+
+    public void check(int[] prices) {
+        for (int i=0; i<this.answer.length; i++) {
+            int targetNumber = this.queue.poll();
+            int count = 0;
+            for (int j=i+1; j<prices.length; j++) {
+                int compareNumber = prices[j];
+                if (targetNumber > compareNumber) {
+                    count++;
+                    break;
+                }
+                if (targetNumber <= compareNumber) {
+                    count++;
+                }
+            }
+            this.answer[i] = count;
+        }
+    }
+
+    public int[] getAnswer() {
+        return this.answer;
+    }
+}
