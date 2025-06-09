3bcab0faa17df34dffc9a55afcc5fd0bac00d26c
juno-choi <ililil9482@naver.com>
2025-06-09 15:43:38 +0900

    이전에 풀이하던 내용 저장

---
+++ src/main/java/kr/co/simol/softeer/test/m1/Main.java
@@ -0,0 +1,25 @@
+package kr.co.simol.softeer.test.m1;
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String input = br.readLine();
+
+            Solution solution = new Solution();
+
+        } catch (Exception e) {
+            System.out.println("input fail");
+        }
+    }
+}
+
+class Solution {
+    public void solution() {
+
+    }
+}
---
+++ src/main/java/kr/co/simol/softeer/test/m3/Main.java
@@ -0,0 +1,24 @@
+package kr.co.simol.softeer.test.m3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String input = br.readLine();
+
+            Solution solution = new Solution();
+
+        } catch (Exception e) {
+            System.out.println("input fail");
+        }
+    }
+}
+
+class Solution {
+    public void solution() {
+
+    }
+}
